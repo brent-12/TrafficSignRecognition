36c6531bce996a1dda5ae1e4f09bef07f2deecd8
Brennon Tiensivu <brennonrt@gmail.com>
2024-10-23 12:50:38 -0400

    Data Recognition

---
+++ data.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+import zipfile
+import os
+import torchvision.transforms as tfrms
+
+# data adjustments for training
+# Resiszing all of the images to 32x32
+
+data_tfrms = tfrms.Compose([
+        tfrms.Resize((32,32)),
+        tfrms.ToTensor(),
+        tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_brightness = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.ColorJitter(brightness=-5),
+    tfrms.ColorJitter(brightness=5),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_saturation = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.ColorJitter(saturation=-5),
+    tfrms.ColorJitter(saturation=5),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+
+])
+
+data_contrast = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.ColorJitter(contrast=-5),
+    tfrms.ColorJitter(contrast=5),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+
+])
+
+data_hue = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.ColorJitter(hue=0.4),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+
+])
+
+data_rotation = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomRotation(15),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_flips = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomHorizontalFlip(1),
+    tfrms.RandomVerticalFlip(1),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_hflip = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomHorizontalFlip(1),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_vflip = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomVerticalFlip(1),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_shear = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomAffine(degrees = 15, shear=2),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_translate = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.RandomAffine(degrees=15, translate=(0.1,0.1)),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_crop = tfrms.Compose([
+    tfrms.Resize((36,36)),
+    tfrms.CenterCrop(32),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
+
+data_grayscale = tfrms.Compose([
+    tfrms.Resize((32,32)),
+    tfrms.Grayscale(num_output_channels=3),
+    tfrms.ToTensor(),
+    tfrms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629)),
+])
