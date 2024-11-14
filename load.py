b28da2832b12a93539c7b163d790968d3d358ebb
Brennon Tiensivu <brennonrt@gmail.com>
2024-11-14 09:59:47 -0500

    deleted unwanted .py files

--- load.py
+++
@@ -1,8 +0,0 @@
-import os
-import cv2
-
-def loadData():
-    images = []
-    classNo = []
-    newList = os.listdir("./train")
-
--- train.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-import numpy as np
-
